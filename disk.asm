8ebd664d3282af432af496155dca3c1ec630eb31
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-10-13 21:51:22 -0300

    clarifying comments

--- disk.asm
+++ disk.asm
@@ -2,7 +2,7 @@ disk_load:
 	pusha
 	push dx
 
-	mov ah, 0x02 ; read mode
+	mov ah, 0x02 ; change to read mode
 	mov al, dh   ; read dh number of sectors
 	mov cl, 0x02 ; start from sector 2
 	             ; (as sector 1 is our boot sector)
@@ -13,7 +13,9 @@ disk_load:
 	; es:bx = buffer pointer is set as input as well
 
 	int 0x13      ; BIOS interrupt
-	jc disk_error ; check carry bit for error
+
+	; if it happens disk errors the BIOS sets cary = 1
+	jc disk_error ; check carry = 1 bit for error
 	 
 	pop dx        ; get back original number of sextors to read
 	cmp al, dh    ; BIOS sets 'al' to the # of sectors actually read
