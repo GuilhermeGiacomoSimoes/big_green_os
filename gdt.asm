4e1ac49956107b99846ce5165199d14612db34cd
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-10-21 21:41:40 -0300

    fix a comment

--- gdt.asm
+++ gdt.asm
@@ -19,7 +19,7 @@ gdt_code:
 gdt_data:
 	dw 0xffff     ; segment length, bits 0-15
 	dw 0x0        ; segment base, bits 0-15
-	db 0x0        ; sefment base, bits 16-23 
+	db 0x0        ; segment base, bits 16-23 
 	db 10010010b  ; flags (8 bits)
 	db 11001111b  ; flags (4 bits) + segment length, bits 16-19
 	db 0x0        ; segment base, bits 24-31
