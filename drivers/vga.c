79778ada475a4b4d4444e08a6681508c088ff806
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-01-17 22:08:48 -0300

    feat: create two functions to set_cursor and get_cursor

--- drivers/vga.c
+++ drivers/vga.c
@@ -1,3 +1,8 @@
+#define VGA_CTRL_REGISTER 0x3d4
+#define VGA_DATA_REGISTER 0x3d5
+#define VGA_OFFSET_LOW 0x0f
+#define VGA_OFFSET_HIGH 0x0e
+
 unsigned char port_byte_in(unsigned short port)
 {
 	unsigned char result;
@@ -8,4 +13,22 @@ unsigned char port_byte_in(unsigned short port)
 void port_byte_out(unsigned short port, unsigned char data)
 {
 	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+void set_cursor(int offset)
+{
+	offset /= 2;
+	port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+	port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset >> 8));
+	port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+	port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset & 0xff));
+}
+
+int get_cursor()
+{
+	port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+	int offset = port_byte_in(VGA_DATA_REGISTER) << 8;
+	port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+	offset += port_byte_in(VGA_DATA_REGISTER);
+	return offset * 2;
 }
