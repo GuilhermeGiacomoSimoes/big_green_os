2621035673cf32aa2071af8bb8ea9baaa4245a08
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-01-17 22:31:54 -0300

    feat: add code to handle with new code

--- drivers/vga.c
+++ drivers/vga.c
@@ -3,6 +3,11 @@
 #define VGA_OFFSET_LOW 0x0f
 #define VGA_OFFSET_HIGH 0x0e
 
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define WHITE_ON_BLACK 0x0f
+
 unsigned char port_byte_in(unsigned short port)
 {
 	unsigned char result;
@@ -31,4 +36,41 @@ int get_cursor()
 	port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
 	offset += port_byte_in(VGA_DATA_REGISTER);
 	return offset * 2;
+}
+
+void set_char_at_video_memory(char character, int offset)
+{
+	unsigned char *vidmem = (unsigned char *) VIDEO_ADDRESS;
+	vidmem[offset] = character;
+	vidmem[offset + 1] = WHITE_ON_BLACK;
+}
+
+int get_row_from_offset(int offset)
+{
+	return offset / (2 * MAX_COLS);
+}
+
+int get_offset(int col, int row)
+{
+	return 2 * (row * MAX_COLS + col);
+}
+
+int move_offset_to_new_line(int offset)
+{
+	return get_offset(0, get_row_from_offset(offset) + 1);
+}
+
+void print_string(char *str)
+{
+	int offset = get_cursor();
+	for(int i =0; str[i] != 0; i++) {
+		if(str[i] == '\n') 
+			offset = move_offset_to_new_line(offset);
+		else {
+			offset += 2;
+			set_char_at_video_memory(str[i], offset);
+		}
+	}
+
+	set_cursor(offset);
 }
