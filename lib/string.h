904918a8cb771067cebd2d627a99453a345ab70b
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-01-19 17:33:45 -0300

    feat: create a strcpy in lib string.h

--- lib/string.h
+++ lib/string.h
@@ -1,4 +1,13 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
+inline void strcpy(char *dest, const char *source)
+{
+	for(
+		int i = 0; 
+		*(dest + i) != '\0'; 
+		i++, *(dest + i) = *(source + i)
+	);
+}
+
 #endif
