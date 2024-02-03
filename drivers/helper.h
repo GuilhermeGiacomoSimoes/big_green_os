d8cd8774f9ba7199c3bc2994ea711b9adb84ea67
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-02-03 12:34:46 -0300

    feat: create helper file to contains functions that is common in all files

---
+++ drivers/helper.h
@@ -0,0 +1,12 @@
+inline static unsigned char port_byte_in(unsigned short port)
+{
+    unsigned char result;
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result;
+}
+
+inline static void port_byte_out(unsigned short port, unsigned char data)
+{
+	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
