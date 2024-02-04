7f2b9c6d6367973114356d4e4a9bd1ba5e15668c
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-02-03 22:39:16 -0300

    feat: create interrupt handler and scan code table

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -124,4 +124,268 @@ void isr_install()
 	set_idt_gate(29, (uint32_t) isr29);
 	set_idt_gate(30, (uint32_t) isr30);
 	set_idt_gate(31, (uint32_t) isr31);
+
+
+	///
+	/// PIC REMAPPING
+	///
+	
+	///ICW1
+	port_byte_out(0x20, 0x11);
+	port_byte_out(0x20, 0x11);
+
+	///ICW2
+	port_byte_out(0x21, 0x20);
+	port_byte_out(0xA1, 0x28);
+
+	///ICW3
+	port_byte_out(0x21, 0x04);
+	port_byte_out(0xA1, 0x02);
+
+	///ICW4
+	port_byte_out(0x21, 0x01);
+	port_byte_out(0xA1, 0x01);
+
+	///OCW1
+	port_byte_out(0x21, 0x0);
+	port_byte_out(0xA1, 0x0);
+
+
+	set_idt_gate(32, (uint32_t) irq0);
+	set_idt_gate(33, (uint32_t) irq1);
+	set_idt_gate(34, (uint32_t) irq2);
+	set_idt_gate(35, (uint32_t) irq3);
+	set_idt_gate(36, (uint32_t) irq4);
+	set_idt_gate(37, (uint32_t) irq5);
+	set_idt_gate(38, (uint32_t) irq6);
+	set_idt_gate(39, (uint32_t) irq7);
+	set_idt_gate(40, (uint32_t) irq8);
+	set_idt_gate(41, (uint32_t) irq9);
+	set_idt_gate(42, (uint32_t) irq10);
+	set_idt_gate(43, (uint32_t) irq11);
+	set_idt_gate(44, (uint32_t) irq12);
+	set_idt_gate(45, (uint32_t) irq13);
+	set_idt_gate(46, (uint32_t) irq14);
+	set_idt_gate(47, (uint32_t) irq15);
+
+	load_idt();
+}
+
+typedef void (*isr_t)(register_t *);
+isr_t interrupt_handlers[256];
+
+void irq_handler(registers_t *r)
+{
+	if(interrupt_handlers[r->int_no] != 0) {
+		isr_t handler = interrupt_handlers[r->int_no];
+		handler(r);
+	}
+
+	port_byte_out(0x20, 0x20); // primary EOI
+	if(r->int_no < 40) {
+		port_byte_out(0xA0, 0x20); //secondary EOI
+	}
+}
+
+void print_letter(uint8_t scancode)
+{
+	switch(scancode) {
+        case 0x0:
+            print_string("ERROR");
+            break;
+        case 0x1:
+            print_string("ESC");
+            break;
+        case 0x2:
+            print_string("1");
+            break;
+        case 0x3:
+            print_string("2");
+            break;
+        case 0x4:
+            print_string("3");
+            break;
+        case 0x5:
+            print_string("4");
+            break;
+        case 0x6:
+            print_string("5");
+            break;
+        case 0x7:
+            print_string("6");
+            break;
+        case 0x8:
+            print_string("7");
+            break;
+        case 0x9:
+            print_string("8");
+            break;
+        case 0x0A:
+            print_string("9");
+            break;
+        case 0x0B:
+            print_string("0");
+            break;
+        case 0x0C:
+            print_string("-");
+            break;
+        case 0x0D:
+            print_string("+");
+            break;
+        case 0x0E:
+            print_string("Backspace");
+            break;
+        case 0x0F:
+            print_string("Tab");
+            break;
+        case 0x10:
+            print_string("Q");
+            break;
+        case 0x11:
+            print_string("W");
+            break;
+        case 0x12:
+            print_string("E");
+            break;
+        case 0x13:
+            print_string("R");
+            break;
+        case 0x14:
+            print_string("T");
+            break;
+        case 0x15:
+            print_string("Y");
+            break;
+        case 0x16:
+            print_string("U");
+            break;
+        case 0x17:
+            print_string("I");
+            break;
+        case 0x18:
+            print_string("O");
+            break;
+        case 0x19:
+            print_string("P");
+            break;
+        case 0x1A:
+            print_string("[");
+            break;
+        case 0x1B:
+            print_string("]");
+            break;
+        case 0x1C:
+            print_string("ENTER");
+            break;
+        case 0x1D:
+            print_string("LCtrl");
+            break;
+        case 0x1E:
+            print_string("A");
+            break;
+        case 0x1F:
+            print_string("S");
+            break;
+        case 0x20:
+            print_string("D");
+            break;
+        case 0x21:
+            print_string("F");
+            break;
+        case 0x22:
+            print_string("G");
+            break;
+        case 0x23:
+            print_string("H");
+            break;
+        case 0x24:
+            print_string("J");
+            break;
+        case 0x25:
+            print_string("K");
+            break;
+        case 0x26:
+            print_string("L");
+            break;
+        case 0x27:
+            print_string(";");
+            break;
+        case 0x28:
+            print_string("'");
+            break;
+        case 0x29:
+            print_string("`");
+            break;
+        case 0x2A:
+            print_string("LShift");
+            break;
+        case 0x2B:
+            print_string("\\");
+            break;
+        case 0x2C:
+            print_string("Z");
+            break;
+        case 0x2D:
+            print_string("X");
+            break;
+        case 0x2E:
+            print_string("C");
+            break;
+        case 0x2F:
+            print_string("V");
+            break;
+        case 0x30:
+            print_string("B");
+            break;
+        case 0x31:
+            print_string("N");
+            break;
+        case 0x32:
+            print_string("M");
+            break;
+        case 0x33:
+            print_string(",");
+            break;
+        case 0x34:
+            print_string(".");
+            break;
+        case 0x35:
+            print_string("/");
+            break;
+        case 0x36:
+            print_string("Rshift");
+            break;
+        case 0x37:
+            print_string("Keypad *");
+            break;
+        case 0x38:
+            print_string("LAlt");
+            break;
+        case 0x39:
+            print_string("Space");
+            break;
+        default:
+            ///'keuyp' event corresponds to the 'keydown' + 0x80
+            ///it may still be a scancode we haven't implemented yet, or
+            ///maybe a control/escape sequence 
+            if (scancode <= 0x7f) {
+                print_string("Unknown key down");
+            } else if (scancode <= 0x39 + 0x80) {
+                print_string("key up ");
+                print_letter(scancode - 0x80);
+            } else print_string("Unknown key up");
+            break;
+    }
+}
+
+static void keyboard_callback(registers_t *regs) 
+{
+	uint8_t scancode = port_byte_in(0x60);
+	print_letter(scancode);
+}
+
+#define IRQ1 33
+void init_keyboard()
+{
+	register_interrupt_handler(IRQ1, keyboard_callback);
 }
