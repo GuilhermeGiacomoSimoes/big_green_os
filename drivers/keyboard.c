0ea65243d60a3e44cf7d7633e556cedca0823e33
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-02-09 23:22:12 -0300

    fix: fix the acces in the port 0x0A

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -203,23 +203,23 @@ void isr_install()
 	/// PIC REMAPPING
 	///
 	
-	///ICW1
+	// ICW1
 	port_byte_out(0x20, 0x11);
-	port_byte_out(0x20, 0x11);
-
-	///ICW2
+	port_byte_out(0xA0, 0x11);
+	
+	// ICW2
 	port_byte_out(0x21, 0x20);
 	port_byte_out(0xA1, 0x28);
-
-	///ICW3
+	
+	// ICW3
 	port_byte_out(0x21, 0x04);
 	port_byte_out(0xA1, 0x02);
-
-	///ICW4
+	
+	// ICW4
 	port_byte_out(0x21, 0x01);
 	port_byte_out(0xA1, 0x01);
-
-	///OCW1
+	
+	// OCW1
 	port_byte_out(0x21, 0x0);
 	port_byte_out(0xA1, 0x0);
 
