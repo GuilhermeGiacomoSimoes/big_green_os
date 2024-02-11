e33d409f0c3d5ee8f3feee3dfad3c506b4690077
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-02-11 15:27:29 -0300

    feat: execute command exit in shell.c

--- client/shell.c
+++ client/shell.c
@@ -1,1 +1,15 @@
+#include "../lib/string.h"
+#include "../drivers/vga.h"
+#include "shell.h"
 
+void execute_command(char *input)
+{
+	if(strcmp(input, "EXIT") == 0) {
+		print_string("Stopping the cpu. Bye\n");
+		asm volatile("hlt");
+	}
+
+	print_string("unknown command: ");
+	print_string(input);
+	print_string("\n>_ ");
+}
