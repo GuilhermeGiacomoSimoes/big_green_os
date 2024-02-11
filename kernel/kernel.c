fff4ba0a8e550471166fd3bb31f79136afbfde15
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-02-11 15:28:26 -0300

    refactor: refactor kernel file to print status messages

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,7 +4,15 @@
 void k_main (void) 
 {
 	clear_screen();
+	print_string("Installing interrupt service routines \n");
 	isr_install();
+
+	print_string("Enabling external interrupts. \n");
 	asm volatile("sti");
+
+	print_string("Initializing keyboard \n");
 	init_keyboard();
+
+	clear_screen();
+	print_string(">_ ");
 }
