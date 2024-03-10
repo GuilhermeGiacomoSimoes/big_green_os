b21b5282a4a6c65ec9df300cf17955446736dfb7
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-03-10 11:07:30 -0300

    feat: create functions to dealloc memory

--- memory/alocated.c
+++ memory/alocated.c
@@ -72,3 +72,48 @@ void *k_malloc(size_t size)
 	return NULL_POINT;
 }
 
+void* __merge_next_node_into_current(
+	dynamic_mem_node_t* current_mem_node)
+{
+	dynamic_mem_node_t *next_mem_node = current_mem_node->next;
+	if(next_mem_node != NULL_POINT && !next_mem_node->used) {
+		current_mem_node->size += current_mem_node->next->size;
+		current_mem_node->size += 16;
+
+		current_mem_node->next = current_mem_node->next->next;
+		if(current_mem_node->next != NULL_POINT) 
+			current_mem_node->next->prev = current_mem_node;
+	}
+
+	return current_mem_node;
+}
+
+void* __merge_current_node_into_previous(
+	dynamic_mem_node_t* current_mem_node)
+{
+	dynamic_mem_node_t *prev_mem_node = current_mem_node->prev;
+	if(prev_mem_node != NULL_POINT && !prev_mem_node->used) {
+		prev_mem_node->size += current_mem_node->size;
+		prev_mem_node->size += 16;
+
+		prev_mem_node->next = current_mem_node->next;
+		if(current_mem_node->next != NULL_POINT) 
+			current_mem_node->next->prev = prev_mem_node;	
+	}
+}
+
+void mem_free(void *p) 
+{
+	if(p == NULL_POINT) return;
+
+	dynamic_mem_node_t *current_mem_node 
+		= (dynamic_mem_node_t *) ((uint8_t *) p - 16);
+
+	if(current_mem_node == NULL_POINT) return;
+
+	current_mem_node->used = 0;
+
+	current_mem_node = __merge_next_node_into_current(current_mem_node);
+	__merge_current_node_into_previous(current_mem_node);
+}
+
--- memory/alocated.h
+++ memory/alocated.h
@@ -1,4 +1,10 @@
 #ifndef __ALOCATED_H__
 #define __ALOCATED_H__
 
+#include<stdint.h>
+#include<stdio.h>
+
+void *k_malloc(size_t size);
+void k_free(void *p);
+
 #endif
