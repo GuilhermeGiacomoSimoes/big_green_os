bb0926a1bf61245464481a32a796becc36aa4944
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-03-16 15:49:33 -0300

    refactor: change to .c file

--- drivers/helper.h
+++ drivers/helper.h
@@ -1,12 +1,3 @@
-inline static unsigned char port_byte_in(unsigned short port)
-{
-    unsigned char result;
-    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
-    return result;
-}
-
-inline static void port_byte_out(unsigned short port, unsigned char data)
-{
-	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
-}
+unsigned char port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
 
