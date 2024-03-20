4879f05975284e717652bb32695e7eff12f33a9c
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-03-20 09:53:17 -0300

    feat: add features

--- arch/x86/io/keyboard.c
+++ arch/x86/io/keyboard.c
@@ -1,5 +1,3 @@
-#pragma once 
-
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -160,7 +158,10 @@ typedef struct {
 
 void isr_handler(registers_t *r)
 {
-	print_string(exception_messages[r->int_no]);
+    print_string("received interrupt: ");
+    char s[3];
+    print_string(s);
+    print_string(exception_messages[r->int_no]);
 }
 
 void load_isr(void)
@@ -261,10 +262,70 @@ void load_irq(void)
 
 void isr_install()
 {
-	load_isr();
-	remapping_pic();
-	load_irq();
-	load_idt();
+   	set_idt_gate(0, (uint32_t) isr0);
+    set_idt_gate(1, (uint32_t) isr1);
+    set_idt_gate(2, (uint32_t) isr2);
+    set_idt_gate(3, (uint32_t) isr3);
+    set_idt_gate(4, (uint32_t) isr4);
+    set_idt_gate(5, (uint32_t) isr5);
+    set_idt_gate(6, (uint32_t) isr6);
+    set_idt_gate(7, (uint32_t) isr7);
+    set_idt_gate(8, (uint32_t) isr8);
+    set_idt_gate(9, (uint32_t) isr9);
+    set_idt_gate(10, (uint32_t) isr10);
+    set_idt_gate(11, (uint32_t) isr11);
+    set_idt_gate(12, (uint32_t) isr12);
+    set_idt_gate(13, (uint32_t) isr13);
+    set_idt_gate(14, (uint32_t) isr14);
+    set_idt_gate(15, (uint32_t) isr15);
+    set_idt_gate(16, (uint32_t) isr16);
+    set_idt_gate(17, (uint32_t) isr17);
+    set_idt_gate(18, (uint32_t) isr18);
+    set_idt_gate(19, (uint32_t) isr19);
+    set_idt_gate(20, (uint32_t) isr20);
+    set_idt_gate(21, (uint32_t) isr21);
+    set_idt_gate(22, (uint32_t) isr22);
+    set_idt_gate(23, (uint32_t) isr23);
+    set_idt_gate(24, (uint32_t) isr24);
+    set_idt_gate(25, (uint32_t) isr25);
+    set_idt_gate(26, (uint32_t) isr26);
+    set_idt_gate(27, (uint32_t) isr27);
+    set_idt_gate(28, (uint32_t) isr28);
+    set_idt_gate(29, (uint32_t) isr29);
+    set_idt_gate(30, (uint32_t) isr30);
+    set_idt_gate(31, (uint32_t) isr31);
+
+    // Remap the PIC
+    port_byte_out(0x20, 0x11);
+    port_byte_out(0xA0, 0x11);
+    port_byte_out(0x21, 0x20);
+    port_byte_out(0xA1, 0x28);
+    port_byte_out(0x21, 0x04);
+    port_byte_out(0xA1, 0x02);
+    port_byte_out(0x21, 0x01);
+    port_byte_out(0xA1, 0x01);
+    port_byte_out(0x21, 0x0);
+    port_byte_out(0xA1, 0x0);
+
+    // Install the IRQs
+    set_idt_gate(32, (uint32_t)irq0);
+    set_idt_gate(33, (uint32_t)irq1);
+    set_idt_gate(34, (uint32_t)irq2);
+    set_idt_gate(35, (uint32_t)irq3);
+    set_idt_gate(36, (uint32_t)irq4);
+    set_idt_gate(37, (uint32_t)irq5);
+    set_idt_gate(38, (uint32_t)irq6);
+    set_idt_gate(39, (uint32_t)irq7);
+    set_idt_gate(40, (uint32_t)irq8);
+    set_idt_gate(41, (uint32_t)irq9);
+    set_idt_gate(42, (uint32_t)irq10);
+    set_idt_gate(43, (uint32_t)irq11);
+    set_idt_gate(44, (uint32_t)irq12);
+    set_idt_gate(45, (uint32_t)irq13);
+    set_idt_gate(46, (uint32_t)irq14);
+    set_idt_gate(47, (uint32_t)irq15);
+
+    load_idt(); 
 }
 
 typedef void (*isr_t)(registers_t *);
@@ -284,10 +345,11 @@ void irq_handler(registers_t *r)
 	///this is required for the PIC to know that 
 	///the interrupt is handled and it can send
 	///further interrupts.
-	port_byte_out(0x20, 0x20); // primary EOI
-	if(r->int_no < 40) {
+	///
+	if(r->int_no >= 40) {
 		port_byte_out(0xA0, 0x20); //secondary EOI
 	}
+	port_byte_out(0x20, 0x20); // primary EOI
 }
 
 ///This map of code -> letter is a specific keyboard..
@@ -324,22 +386,25 @@ const char scancode2char[] = {
 };
 static void keyboard_callback(registers_t *regs) 
 {
-	const uint8_t scancode = port_byte_in(0x60);
-	if(scancode > SC_MAX) return;
+	uint8_t scanmode = port_byte_in(0x60);
+	///print_string(scanmode);
 
-	if(scancode == BACKSPACE) {
-		if(backspace(key_buff)) 
-			print_backspace();
-	} else if (scancode == ENTER) {
-		///print_nl();
-		execute_command(key_buff);
-		key_buff[0] = '\0';
-	} else {
-		char letter = scancode2char[(int) scancode];
-		append(key_buff, letter);
-		char str[2] = {letter, '\0'};
-		print_string(str);
-	}
+	///const uint8_t scancode = port_byte_in(0x60);
+	///if(scancode > SC_MAX) return;
+
+	///if(scancode == BACKSPACE) {
+	///	if(backspace(key_buff)) 
+	///		print_backspace();
+	///} else if (scancode == ENTER) {
+	///	///print_nl();
+	///	execute_command(key_buff);
+	///	key_buff[0] = '\0';
+	///} else {
+	///	char letter = scancode2char[(int) scancode];
+	///	append(key_buff, letter);
+	///	char str[2] = {letter, '\0'};
+	///	print_string(str);
+	///}
 }
 
 void register_interrupt_handler(uint8_t n, isr_t handler)
