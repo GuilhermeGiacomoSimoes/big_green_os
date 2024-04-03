c4df6c25378ba8eb8636c468a9fc0eda0b893540
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2024-04-02 23:20:05 -0300

    refactor: add __ prefix in internal functions

--- arch/x86/io/keyboard.c
+++ arch/x86/io/keyboard.c
@@ -81,7 +81,7 @@ typedef struct {
 idt_gate_t idt[256];
 
 idt_register_t idt_reg;
-void load_idt() 
+void __load_idt() 
 {
 	idt_reg.base = (uint32_t) &idt;
 	const int idt_entries = 256;
@@ -167,7 +167,7 @@ void isr_handler(registers_t *r)
     print_string(exception_messages[r->int_no]);
 }
 
-void load_isr(void)
+void __load_isr(void)
 {
 	set_idt_gate(0, (uint32_t) isr0);
 	set_idt_gate(1, (uint32_t) isr1);
@@ -243,7 +243,7 @@ void remapping_pic(void)
 	port_byte_out(0xA1, 0x0);
 }
 
-void load_irq(void)
+void __load_irq(void)
 {
 	set_idt_gate(32, (uint32_t) irq0);
 	set_idt_gate(33, (uint32_t) irq1);
@@ -263,42 +263,8 @@ void load_irq(void)
 	set_idt_gate(47, (uint32_t) irq15);
 }
 
-void isr_install()
+void __remap_the_pic(void)
 {
-   	set_idt_gate(0, (uint32_t) isr0);
-    set_idt_gate(1, (uint32_t) isr1);
-    set_idt_gate(2, (uint32_t) isr2);
-    set_idt_gate(3, (uint32_t) isr3);
-    set_idt_gate(4, (uint32_t) isr4);
-    set_idt_gate(5, (uint32_t) isr5);
-    set_idt_gate(6, (uint32_t) isr6);
-    set_idt_gate(7, (uint32_t) isr7);
-    set_idt_gate(8, (uint32_t) isr8);
-    set_idt_gate(9, (uint32_t) isr9);
-    set_idt_gate(10, (uint32_t) isr10);
-    set_idt_gate(11, (uint32_t) isr11);
-    set_idt_gate(12, (uint32_t) isr12);
-    set_idt_gate(13, (uint32_t) isr13);
-    set_idt_gate(14, (uint32_t) isr14);
-    set_idt_gate(15, (uint32_t) isr15);
-    set_idt_gate(16, (uint32_t) isr16);
-    set_idt_gate(17, (uint32_t) isr17);
-    set_idt_gate(18, (uint32_t) isr18);
-    set_idt_gate(19, (uint32_t) isr19);
-    set_idt_gate(20, (uint32_t) isr20);
-    set_idt_gate(21, (uint32_t) isr21);
-    set_idt_gate(22, (uint32_t) isr22);
-    set_idt_gate(23, (uint32_t) isr23);
-    set_idt_gate(24, (uint32_t) isr24);
-    set_idt_gate(25, (uint32_t) isr25);
-    set_idt_gate(26, (uint32_t) isr26);
-    set_idt_gate(27, (uint32_t) isr27);
-    set_idt_gate(28, (uint32_t) isr28);
-    set_idt_gate(29, (uint32_t) isr29);
-    set_idt_gate(30, (uint32_t) isr30);
-    set_idt_gate(31, (uint32_t) isr31);
-
-    // Remap the PIC
     port_byte_out(0x20, 0x11);
     port_byte_out(0xA0, 0x11);
     port_byte_out(0x21, 0x20);
@@ -309,26 +275,14 @@ void isr_install()
     port_byte_out(0xA1, 0x01);
     port_byte_out(0x21, 0x0);
     port_byte_out(0xA1, 0x0);
+}
 
-    // Install the IRQs
-    set_idt_gate(32, (uint32_t)irq0);
-    set_idt_gate(33, (uint32_t)irq1);
-    set_idt_gate(34, (uint32_t)irq2);
-    set_idt_gate(35, (uint32_t)irq3);
-    set_idt_gate(36, (uint32_t)irq4);
-    set_idt_gate(37, (uint32_t)irq5);
-    set_idt_gate(38, (uint32_t)irq6);
-    set_idt_gate(39, (uint32_t)irq7);
-    set_idt_gate(40, (uint32_t)irq8);
-    set_idt_gate(41, (uint32_t)irq9);
-    set_idt_gate(42, (uint32_t)irq10);
-    set_idt_gate(43, (uint32_t)irq11);
-    set_idt_gate(44, (uint32_t)irq12);
-    set_idt_gate(45, (uint32_t)irq13);
-    set_idt_gate(46, (uint32_t)irq14);
-    set_idt_gate(47, (uint32_t)irq15);
-
-    load_idt(); 
+void isr_install()
+{
+	__load_isr();
+	__remap_the_pic();
+	__load_irq();
+    __load_idt(); 
 }
 
 typedef void (*isr_t)(registers_t *);
@@ -355,12 +309,6 @@ void irq_handler(registers_t *r)
 	port_byte_out(0x20, 0x20); // primary EOI
 }
 
-///This map of code -> letter is a specific keyboard..
-///IBM PC PS/2
-///
-///Each keyboard has your map
-///
-
 bool backspace(char buff[])
 {
 	const int len = strlen(buff); 
@@ -376,6 +324,12 @@ bool backspace(char buff[])
 #define ENTER 0x1c
 
 static char key_buff[256];
+
+///This map of code -> letter is a specific keyboard..
+///IBM PC PS/2
+///
+///Each keyboard has your map
+///
 const char scancode2char[] = {
   '?', '?', '1', '2', '3', '4', '5',
   '6', '7', '8', '9', '0', '-', '=',
