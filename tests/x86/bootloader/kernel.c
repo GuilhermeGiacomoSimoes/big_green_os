812ec19451682505d347a6be8a1940543dd4bfef
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2024-06-09 10:14:07 -0300

    refactor: remove uneccessary functions

--- tests/x86/bootloader/kernel.c
+++ tests/x86/bootloader/kernel.c
@@ -1,22 +1,13 @@
-int str_size(char *str) {
-	int size = 0;
-	for( ; *(str + size) != '\0' ; ++ size );
-	return size;
-}
-
-void print_str(char *str)
+void main()
 {
-	int size = str_size(str);
+	char msg[] = {'B', 'O', 'O', 'T', ' ', 'I', 'S', ' ', 'W', 'O', 'R', 'K', 0};
+
+	int size = 0;
+	for( ; *(msg + size) != '\0' ; ++ size );
 
 	unsigned char * vid_mem = (unsigned char *) 0xb8000;
 
 	for (int i = 0 ; i < size ; i ++ ) {
-		*(vid_mem + i * 2) = str[i];
+		*(vid_mem + i * 2) = msg[i];
 	}
-}
-
-void main()
-{
-	char msg[] = {'B', 'O', 'O', 'T', ' ', 'I', 'S', ' ', 'W', 'O', 'R', 'K', 0};
-	print_str(msg);
 }
